b83fd28d6d254569c1ee6610ca4f5d5e9f9c4e34
knika-starkey <knika787@gmail.com>
2021-12-11 11:18:52 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let tick = true;
+let t;
+function ticker(time) {
+  let d1 = new Date();
+  let dif = (d1.getTime() - d.getTime()) / 1000;
+  if (dif >= time) {
+    clearInterval(t);
+  }
+  if (tick) {
+    document.write("Тик ");
+    tick = false;
+  } else {
+    document.write("Так ");
+    tick = true;
+  }
+}
+
+let d = new Date();
+//let time;
+t = setInterval(ticker.bind(this, 10), 1000);
